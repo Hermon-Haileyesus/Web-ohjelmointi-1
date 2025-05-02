7614f5bb5fc624786ed21539a85d1b1af1aecaa5
Hermon <hg2397@edu.turku.fi>
2025-05-02 09:02:45 +0300

    I made javascript for html file

---
+++ portfolio/muuttuja.js
@@ -0,0 +1,21 @@
+let a = 3;
+let b = 5;
+let c = 7;
+let d = 15;
+
+
+function summa() {
+    alert("Summa: " + (a + b));
+}
+
+function vahennys() {
+    alert("Vähennys: " + (b - c));
+}
+
+function kerto() {
+    alert("Kerto: " + (c * d));
+}
+
+function jako() {
+    alert("Jako: " + (d / a));
+}
